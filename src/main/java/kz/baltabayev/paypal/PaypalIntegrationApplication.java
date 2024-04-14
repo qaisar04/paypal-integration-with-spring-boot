670b4e7cb2fa41ea41d88c9fe6e03061c898ff0a
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-04-14 16:19:22 +0500

    init

---
+++ src/main/java/kz/baltabayev/paypal/PaypalIntegrationApplication.java
@@ -0,0 +1,13 @@
+package kz.baltabayev.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalIntegrationApplication.class, args);
+    }
+
+}
